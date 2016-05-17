ee02346413daf6c051146af20c233e2cd12ff1e6
withrocks <withrocks@users.noreply.github.com>
2016-05-17 15:13:27 +0200

    Add the units class
    - Currently capable of mapping between nano and pico.
      Other units can be added when needed - skipped for now to limit
      necessary testing.
    - Unit tests

---
+++ clarity_ext/units.py
@@ -0,0 +1,30 @@
+import math
+import logging
+
+
+class Units:
+    NANO = -9
+    PICO = -12
+
+    MAPPING = {
+        NANO: "n",
+        PICO: "p"
+    }
+
+    def __init__(self, logger=None):
+        self.logger = logger or logging.getLogger(__name__)
+
+    def convert(self, value, unit_from, unit_to):
+        if unit_from == unit_to:
+            return value
+
+        factor = math.pow(10, unit_from - unit_to)
+        ret = value * factor
+        if self.logger.isEnabledFor(logging.DEBUG):
+            self.logger.debug("Original: {} {}, Factor: {}, New: {} {}".format(
+                value, self.MAPPING[unit_from], factor, ret, self.MAPPING[unit_to]))
+        return ret
+
+    def unit_to_string(self, unit):
+        return self.MAPPING[unit]
+
