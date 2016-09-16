2899587ee5e30c6a4504f4ab9413149306410654
GitEdvard <edvard.englund@medsci.uu.se>
2016-09-16 10:50:26 +0200

    Link shared file name to the specific integration test that is given
    Shared file name for a pre made integration test step is bound to a specific date and initials. In production, the date and initials should be generated.

--- clarity_ext/extensions.py
+++ clarity_ext/extensions.py
@@ -56,6 +56,18 @@ class ExtensionService(object):
         else:
             return in_argument.__dict__
 
+    def _fetch_shared_file(self, extension, run_arguments):
+        """
+        Fetch shared file name from
+        1. run_arguments, if the entry "shared_file" exists (integration tests)
+        2. the extension (production)
+        Thus, entry "shared_file" in run_argument overrides the shared file from extension
+        """
+        if "shared_file" in run_arguments:
+            return run_arguments["shared_file"]
+        else:
+            return extension.shared_file()
+
     def execute(self, module, mode, run_arguments_list=None, config=None, artifacts_to_stdout=True,
                 print_help=True, use_cache=None):
         """
@@ -151,7 +163,8 @@ class ExtensionService(object):
                 instance = extension(context)
                 os_service = OSService()
                 if issubclass(extension, DriverFileExtension):
-                    file_svc = DriverFileService.create_file_service(instance, self.logger, os_service)
+                    shared_file_name = self._fetch_shared_file(instance, run_arguments)
+                    file_svc = DriverFileService.create_file_service(instance, shared_file_name, self.logger, os_service)
                     commit = mode == self.RUN_MODE_EXEC
                     file_svc.execute(commit=commit, artifacts_to_stdout=artifacts_to_stdout)
                 elif issubclass(extension, GeneralFileExtension):
@@ -313,6 +326,7 @@ class DriverFileExtension(GeneralFileExtension):
     def content(self):
         """Yields the output lines of the file, or the response at updates"""
         pass
+
 
 class ExtensionTest(object):
     def __init__(self, pid):
