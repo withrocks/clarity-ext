b8e112d7dfa5636d860c1099e8b0498bec47005a
withrocks <withrocks@users.noreply.github.com>
2017-03-09 12:35:40 +0100

    Throw errors when handling validation exceptions containing errors

--- clarity_ext/service/validation_service.py
+++ clarity_ext/service/validation_service.py
@@ -1,5 +1,4 @@
-from clarity_ext.service.step_logger_service import StepLoggerService
-from clarity_ext.domain.validation import ValidationType
+from clarity_ext.domain.validation import ValidationType, UsageError
 
 
 class ValidationService:
@@ -16,9 +15,11 @@ class ValidationService:
             self._log_debug("Validation errors, len = {}".format(len(results)))
             for result in results:
                 self.handle_single_validation(result)
+        # If any of the validation results were errors, raise an exception:
+        if any(result for result in results if result.type == ValidationType.ERROR):
+            raise UsageError("Errors during validation. See the step log for further details.", results)
 
     def handle_single_validation(self, result):
-        # TODO: If warning, log, if error, log, then throw
         msg_row = "{}".format(result)
         self.step_logger_service.log(msg_row)
         self._log_debug("{}".format(msg_row))
